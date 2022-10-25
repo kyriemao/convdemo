4570a04153e4a6672e9e97299c9509b07e9317e1
kyriemao <kyriemao@foxmail.com>
2022-10-25 10:48:25 +0800

    add minimal home page

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import url_for
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello'
+
+@app.route('/user/<good>')
+def user_page(name):
+    return f'User: {escape(name)}'
+
+@app.route('/test')
+def test_url_for():
+    # 下面是一些调用示例（请访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
+    print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
+    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
+    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
+    print(url_for('user_page', name='peter'))  # 输出：/user/peter
+    print(url_for('test_url_for'))  # 输出：/test
+    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
+    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
+    return 'Test page'
