aa647939aff7cd64c3bf1a0e5cf2c4ca73bb06ce
kyriemao <kyriemao@foxmail.com>
2022-10-25 16:07:19 +0800

    template

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ from markupsafe import escape
 from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
 import os
 
+
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
 
+
+# database
 db = SQLAlchemy(app) 
 
 class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
@@ -18,6 +22,9 @@ class Movie(db.Model):  # 表名将会是 movie
     id = db.Column(db.Integer, primary_key=True)  # 主键
     title = db.Column(db.String(60))  # 电影标题
     year = db.Column(db.String(4))  # 电影年份
+
+
+
 
 import click
 @app.cli.command()
@@ -50,13 +57,18 @@ def forge():
     click.echo('Done.')
 
 
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
 
 @app.route('/')
 def index():
-    user = User.query.first()  # 读取用户记录
-    print(user.name)
     movies = Movie.query.all()  # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html',  movies=movies)
 
 
-    
+
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    return render_template('404.html'), 404  # 返回模板和状态码
