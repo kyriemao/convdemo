80c57bce9ac164ba8ecbb4c50c131339d1300ada
kyriemao <kyriemao@foxmail.com>
2022-10-26 14:31:32 +0800

    write rewriter

--- models.py
+++ models.py
@@ -1,9 +1,46 @@
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
 class Rewriter:
-    def __init__(self, model_path):
-        pass
+    def __init__(self, model_path, device):
+        self.tokenizer = T5Tokenizer.from_pretrained(model_path)
+        self.t5 = T5ForConditionalGeneration.from_pretrained(model_path)
+        self.t5.to(device)
+        self.max_response_length = 64
+        self.max_query_length = 32
+        self.max_seq_length = 256
+
+    def rewrite(self, query, context):
+        input_ids = []
+        encoded_query = self.tokenizer.encode(query,
+                                              add_special_tokens=True, 
+                                              max_length=self.max_query_length, 
+                                              truncation=True)
+        input_ids.extend(encoded_query)
+        last_response = context[-1]
+        encoded_response = self.tokenizer.encode(last_response, 
+                                                 add_special_tokens=True, 
+                                                 max_length=self.max_response_length, 
+                                                 truncation=True)[1:] # remove [CLS]
+        input_ids.extend(encoded_response)
+        
+        for i in range(len(context) - 2, -1, -2):
+            encoded_history = self.tokenizer.encode(context[i],
+                                                    add_special_tokens=True, 
+                                                    max_length=self.max_query_length, 
+                                                    truncation=True)[1:] # remove [CLS]
+            if len(input_ids) + len(encoded_history) > self.max_seq_length:
+                break
+            input_ids.extend(encoded_history)
+        
+        input_ids = input_ids.to(self.device)
+        outputs = self.t5.generate(input_ids)
+        rewrite_text = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        return rewrite_text
+
 
 class Retriever:
-    pass
+    def __init__(self) -> None:
+        pass
 
 class Reranker:
     pass
--- myapp.py
+++ myapp.py
@@ -6,65 +6,11 @@ from flask_login import LoginManager, login_user
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from bot import search_bot
+import torch
+import argparse
+from models import Rewriter, Retriever, Reranker 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dev'
-# prefix = 'sqlite:////'
-# app.config['SQLALCHEMY_DATABASE_URI'] =  prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# db = SQLAlchemy(app)
-# login_manager = LoginManager(app)
-# login_manager.login_view = 'login'
-# login_manager.login_message = 'Welcome to our conversational search bot!'
-
-
-# class User(db.Model, UserMixin):
-#     user_id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(20))  # 用户名
-#     password_hash = db.Column(db.String(128))  # 密码散列值
-#     dialog_history = db.Column(db.LONGTEXT) 
-#     def set_password(self, password):  # 用来设置密码的方法，接受密码作为参数
-#         self.password_hash = generate_password_hash(password)  # 将生成的密码保持到对应字段
- 
-#     def validate_password(self, password):  # 用于验证密码的方法，接受密码作为参数
-#         return check_password_hash(self.password_hash, password)  # 返回布尔值
-
-
-# @login_manager.user_loader
-# def load_user(user_id): 
-#     user = User.query.get(int(user_id))
-#     return user
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         username = request.form['username']
-#         password = request.form['password']
-
-#         if not username or not password:
-#             flash('Invalid input.')
-#             return redirect(url_for('login'))
-
-#         user = User.query.first()
-#         if username == user.username and user.validate_password(password):
-#             login_user(user)
-#             flash('Login success.')
-#             return redirect(url_for('index'))
-
-#         flash('Invalid username or password.')
-#         return redirect(url_for('login'))
-
-#     return render_template('login.html')
-
-
-# @app.route('/logout')
-# @login_required
-# def logout():
-#     logout_user()
-#     flash('Goodbye.')
-#     return redirect(url_for('index'))
-
 
 @app.route("/")
 def home():
@@ -72,7 +18,6 @@ def home():
 
 
 context = []
-
 @app.route("/get")
 def get_bot_response():
 	user_query = request.args.get('msg')
@@ -83,5 +28,21 @@ def get_bot_response():
 	return response
 
 
+def get_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--rewriter_path", type=str, default="")
+	parser.add_argument("--retriever_path", type=str, default="")
+	parser.add_argument("--reranker_path", type=str, default="")
+	parser.add_argument("--index_path", type=str, default="")
+	parser.add_argument("--mode", type=str, required=True, choices=["cqr", "cdr"])
+
+	args = parser.parse_args()
+	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+	args.device = device
+	return args
+
+
 if __name__ == "__main__":
+	args = get_args()
+	
 	app.run()
