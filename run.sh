cf246f25de5326ea9c500e0ddba3c7ec4df5fb1e
kyriemao <57164831+kyriemao@users.noreply.github.com>
2022-11-04 00:01:15 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-export CUDA_VISIBLE_DEVICES=5
-
-python myapp.py --mode="cqr" \
---rewriter_path="/data1/kelong_mao/pretrained_models/t5qr_trained_on_qrecc" \
---index_path="/data1/kelong_mao/indexes/cast/ance/doc_embeddings" \
---retriever_path="/data1/kelong_mao/pretrained_models/ance-msmarco" \
---retriever_type="dense" \
---reranker_path="cross-encoder/ms-marco-MiniLM-L-12-v2" \
---collection_path="/data1/kelong_mao/collections/cast/cast_collection/raw.tsv" 
